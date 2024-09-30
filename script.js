9ce6771aee83031a19a1e373b293719c3d70d631
Julia Akemy <144185022+Akemyk@users.noreply.github.com>
2024-09-30 20:20:09 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+let cotacaoDolar = Number(prompt("Digite o valor da cotação do dólar"))
+let qtdDolar = Number(prompt("Digite a quantidade de dólares disponível com o usuário"))
+let moedaBr = qtdDolar/cotacaoDolar
+alert("Dólar para a moeda brasileira é R$"+moedaBr)
